d4638f8a36952e6cec348596914bc01095bd495e
Saptrishi379 <niranjan@saptrishionline.com>
2025-11-02 23:15:51 +0530

    add app.html

--- obj/Debug/net8.0/MySimpleMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MySimpleMvcApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySimpleMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b53610b89736d78bfac4b74bc634084c9bea5cfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d4050104f4416f9c244e7dc6763c269de5ffc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySimpleMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySimpleMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
